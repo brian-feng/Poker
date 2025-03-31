720fb02437f9df26c56a554566c2f54f47e1fdda
brain.exe <brainfeng135@gmail.com>
2025-03-31 03:07:20 -0700

    Cards comparable and hands can be more fluidly edited outside of class

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 
 /// <summary>
-/// The enum for Suites in the backend. Ordered from smallest to largest to accomodate sorting and/or comparison of equally valued cards. Diamonds < Clubs < Hearts < Spades.
+/// The enum for a card's suite. Ordered from smallest to largest to accomodate sorting and/or comparison of equally valued cards. Diamonds < Clubs < Hearts < Spades.
 /// </summary>
 public enum Suite {Diamonds, Clubs, Hearts, Spades};
 
 /// <summary>
-/// The base class for any card in the backend. Values of 1 represent Aces, 11 represent Jacks, 12 represent Queens, and 13 represent Kings.
+/// The component class for any card in the backend. Values of 1 represent Aces, 11 represent Jacks, 12 represent Queens, and 13 represent Kings.
 /// </summary>
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IComparable
 {   
     public int value;
     public Suite suite;
+
+    /// <summary>
+    /// Implementation of CompareTo for Card. Prioritizes values then suites. However, 1 is considered to be the largest value.
+    /// </summary>
+    /// <param name="obj">The card to be compared to.</param>
+    public int CompareTo(object obj){
+        if(obj == null){
+            return 1;
+        }
+        Card otherCard = obj as Card;
+
+        // If obj is a valid Card
+        if(otherCard != null){
+            // 1 is considered to be larger than 13
+            if((value - 2) % 13 == (otherCard.value - 2) % 13){
+                return suite - otherCard.suite;
+            }
+            return (value - 2) % 13 - (otherCard.value - 2) % 13;
+        }
+        else{
+            throw new ArgumentException("Object is not a Card");
+        }
+    }
 }
--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,19 +4,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 
+/// <summary>
+/// The backend hand class. This class prevents cards from exceeding a count of 5 and will log an error when attempting to add to a full hand.
+/// </summary>
 public class Hand {
-    public List<Card> cards;
-    public int value;
+
+    private List<Card> _cards;
+    private int _value = 0;
     public Hand(List<Card> newCards){
-        cards = newCards;
+        foreach(Card card in newCards){
+            AddCard(card);
+        }
     }
     public Hand(){
-        cards = new List<Card>();
+        _cards = new List<Card>();
     }
+
+    // ----- Hand manipulation utility functions ------
+
+    /// <summary>
+    /// Removes all cards from the hand.
+    /// </summary>
+    public void DeleteHand(){
+        _cards.Clear();
+    }
+
+    /// <summary>
+    /// Removes the target card from the hand once.
+    /// </summary>
+    /// <param name="target">The card to be removed.</param>
+    public void DeleteCard(Card target){
+        foreach(Card card in _cards){
+            if(card.CompareTo(target) == 0){
+                _cards.Remove(card);
+                return;
+            }
+        }
+    }
+
+    public void DeleteCards(List<Card> targets){
+        foreach(Card target in targets){
+            DeleteCard(target);
+        }
+    }
+
+    /// <summary>
+    /// Adds a single card to the hand. Will log an error when attempting to add a card to a full hand.
+    /// </summary>
+    /// <param name="newCard">The card to be added.</param>
+    public void AddCard(Card newCard){
+        if(_cards.Count >= 5){
+            Debug.LogWarning("Attempted to add more than 5 cards to hand!");
+            return;
+        }
+        _cards.Add(newCard);
+    }
+
+    /// <summary>
+    /// Adds all elements from parameter into the hand. Will log an error when attempting to add a card to a full hand.
+    /// </summary>
+    /// <param name="newCards">The list of cards to be added.</param>
+    public void AddCards(List<Card> newCards){
+        foreach(Card card in newCards){
+            if(_cards.Count >= 5){
+                Debug.LogWarning("Attempted to add more than 5 cards to hand!");
+                return;
+            }
+            _cards.Add(card);
+        }
+    }
+
+    /// <summary>
+    /// Empties current hand and adds all elements of newCards to the hand. Will log an error when attempting to add a card to a full hand.
+    /// </summary>
+    /// <param name="newCards">The of cards to replace the old hand.</param>
+    public void RefreshHand(List<Card> newCards){
+        DeleteHand();
+        foreach(Card card in newCards){
+            if(_cards.Count >= 5){
+                Debug.LogWarning("Attempted to add more than 5 cards to hand!");
+                return;
+            }
+            _cards.Add(card);
+        }
+    }
+
+    // ----- Hand calculation functions ------
 
     public int countOf(int target){
         int count = 0;
-        foreach (Card card in cards){
+        foreach (Card card in _cards){
             if(card.value == target){
                 count++;
             }
@@ -25,11 +102,15 @@ public class Hand {
     }
 
     public bool contains(int target){
-        foreach (Card card in cards){
+        foreach (Card card in _cards){
             if(card.value == target){
                 return true;
             }
         }
         return false;
+    }
+
+    public bool isFlush(){
+        
     }
 }
